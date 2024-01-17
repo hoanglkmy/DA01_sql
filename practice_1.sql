53446da217098a55b958ae7406c2c898cad06cf9
hoanglkmy <156898731+hoanglkmy@users.noreply.github.com>
2024-01-17 23:54:23 +0700

    Updated Practice_1.sql

--- practice_1.sql
+++ practice_1.sql
@@ -33,3 +33,32 @@ order by name
 select name from employee
 where salary >2000 and months <10
 order by employee_id ASC
+
+--ex9
+select product_id from products
+where low_fats = 'Y' and recyclable = 'y'
+
+--ex10
+select name from customer
+where referee_id <> 2 or referee_id is null
+
+--ex11
+select name, population, area from World
+where area >=3000000 or population >=25000000
+
+--ex12
+select distinct author_id as id from Views
+where viewer_id = author_id
+order by author_id ASC
+
+--ex13
+SELECT part, assembly_step FROM parts_assembly
+where finish_date is null
+
+--ex14
+select * from lyft_drivers
+where yearly_salary <= 30000 or yearly_salary >= 70000
+
+--ex15
+select * from uber_advertising
+where money_spent >= 100000 and year = 2019
