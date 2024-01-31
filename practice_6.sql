e9e0b684e5b860f24250f32d030fd35b29559da6
hoanglkmy <156898731+hoanglkmy@users.noreply.github.com>
2024-02-01 00:24:23 +0700

    Update practice_6.sql

--- practice_6.sql
+++ practice_6.sql
@@ -16,10 +16,19 @@ select category, product, total from new
 limit 4
 
   --Câu hỏi: anh ơi, bài này em đang bị mắc kẹt ở đoạn chọn ra 2 sản phẩm có doanh thu cao nhất ở mỗi loại ạ huhu
---ex3
+--ex3 Lỗi hay sao đó ạ, em chạy thử những câu lệnh đơn giản cx k ra ạ
+  SELECT policy_holder_id, count(case_id) FROM callers
+group by policy_holder_id
 
 --ex4
-
+--Cách 1: join
+  
+SELECT a.page_id
+FROM pages as a
+left join page_likes as b
+on a.page_id=b.page_id
+where b.user_id is null
+order by a.page_id
 --ex5
 
 -ex6
