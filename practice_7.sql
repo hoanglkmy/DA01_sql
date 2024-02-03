f2bb39c24868a22237969562a80f032eb70e6fa1
hoanglkmy <156898731+hoanglkmy@users.noreply.github.com>
2024-02-04 06:00:21 +0700

    Update practice_7.sql

--- practice_7.sql
+++ practice_7.sql
@@ -17,12 +17,36 @@ from monthly_cards_issued) as T
 where T.rank = 1
 order by  issued_amount DESC
 
---ex3
+--ex3 the third transaction of every user
+select user_id, spend, transaction_date from 
+(select *, row_number() over (PARTITION BY user_id order by transaction_date)
+as rank
+from transactions) as T
+where T.rank=3
 
 --ex4
+select  transaction_date, user_id, purchase_count from
+(select user_id, transaction_date,
+count(product_id) over(PARTITION BY user_id order by transaction_date DESC) as purchase_count,
+row_number() over(PARTITION BY user_id order by transaction_date DESC) as rank
+from user_transactions) as T
+where T.rank=1
+order by  transaction_date
 
 --ex5
+select user_id, tweet_date, 
+(tweet_count+second_count+third_count)/3 as rolling_avg_3d
+from
+(select *, 
+lead(second_day) over(partition by user_id order by tweet_date) as third_day,
+lead(second_count) over(partition by user_id order by tweet_date) as third_count
+from 
+(select user_id, tweet_date, tweet_count, 
+lead(tweet_date) over(partition by user_id order by tweet_date) as second_day,
+lead(tweet_count) over(partition by user_id order by tweet_date) as second_count
+from tweets) as second) as third
 
+  --Caau hỏi: Em chưa hiểu đề bài này lắm ạ, em tưởng là tính TB của 3 ngày nhưng mà so với example output thì không phải ạ
 --ex6
 
 --ex7
