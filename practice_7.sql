c5a76ca1e074c0c259989f936c744f08dd325505
hoanglkmy <156898731+hoanglkmy@users.noreply.github.com>
2024-02-04 14:54:19 +0700

    Update practice_7.sql

--- practice_7.sql
+++ practice_7.sql
@@ -48,7 +48,64 @@ from tweets) as second) as third
 
   --Caau hỏi: Em chưa hiểu đề bài này lắm ạ, em tưởng là tính TB của 3 ngày nhưng mà so với example output thì không phải ạ
 --ex6
+select count(*) from 
+(select transaction_id from 
+(select *, 
+lag(credit_card_id) over(PARTITION BY merchant_id, credit_card_id order by merchant_id) as pre_credit_card,
+lag(amount) over(PARTITION BY merchant_id, credit_card_id order by merchant_id) as pre_amount,
+lag(transaction_timestamp) over(PARTITION BY merchant_id, credit_card_id order by merchant_id) as pre_time
+from transactions) as T
+where T.pre_credit_card - T.credit_card_id = 0   and 
+	T.pre_amount- T.amount=0  and date(T.pre_time) = date(T.transaction_timestamp) AND
+	 (abs((extract(hour from T.pre_time)*60+extract(minute from pre_time)) - (extract(hour from transaction_timestamp)*60+extract(minute from transaction_timestamp))) <= 10)) as M 
+
 
 --ex7
+  --Cách 1
+with appliance as (
+select category, product, sum(spend) as total_spend from product_spend
+where category = 'appliance' and extract(year from transaction_date)  = 2022
+group by product, category
+order by total_spend DESC
+limit 2),
+electronics as (
+select category, product, sum(spend) as total_spend from product_spend
+where category = 'electronics' and extract(year from transaction_date)  = 2022
+group by product, category
+order by total_spend DESC
+limit 2)
+
+select * from appliance
+UNION ALL
+select * from electronics
+
+  --Cách 2 Window Function
+select category, product, total from 
+(select category, product, total, 
+dense_rank() over (partition by category order by total DESC) as rank
+from
+(select category, product,
+sum(spend) as total from product_spend
+where extract(year from transaction_date) = '2022'
+group by  category, product) as M
+) as T
+where T.rank in ('1', '2')
 
 --ex8
+with cte AS
+(select a.artist_id, a.artist_name, b.song_id, c.rank
+from artists as a
+join songs as b on a.artist_id=b.artist_id
+join global_song_rank as c on c.song_id=b.song_id),
+
+cte2 as (select artist_name, 
+count(*) as count
+from cte
+where rank <=10
+group by artist_name),
+
+cte3 as(select artist_name,
+dense_rank() over (order by cte2.count DESC) as artist_rank
+from cte2)
+select * from cte3
+where artist_rank <=5
